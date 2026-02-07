abd484d69dd3f56a41cd4a20bb54c6c2058ce66e
aadityaraj2532 <adityajnvbanka15@gmail.com>
2026-02-07 12:42:08 +0530

    Fix Render Blueprint URL property and configure proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    // When running on Render, accessing backend via internal private network
+    const backendUrl = process.env.BACKEND_HOST ? `http://${process.env.BACKEND_HOST}` : 'http://127.0.0.1:8000';
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
